94d41fbd0d09b9354d84d2a18401c537bbebb722
dejourford <dejourford@my.unt.edu>
2023-02-02 00:48:25 -0600

    add style to page and setup api fetch

---
+++ script.js
@@ -0,0 +1,27 @@
+// create variables
+const dateBtn = document.querySelector('.dateBtn')
+const image = document.querySelector('.image')
+const info = document.querySelector('.info')
+
+
+// create event listener for date button
+dateBtn.addEventListener('click', getImage) 
+    
+
+
+
+function getImage() {
+    console.log('nothing yet')
+
+    // fetch nasa api
+    fetch('https://api.nasa.gov/planetary/apod?api_key=UIJQ1JX6609xd4NrstCSef75F1F8N0N6u23UFxS1')
+    .then( response => response.json())
+    .then (data => {
+        console.log(data)
+        image.src = data.hdurl
+        info.textContent = data.explanation
+    })
+    .catch( error => {
+        console.log(`error${error}`)
+    })
+}
